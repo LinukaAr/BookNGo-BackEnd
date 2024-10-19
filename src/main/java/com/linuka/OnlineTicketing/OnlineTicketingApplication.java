8196074f46672496c0d54a38e03626477fda58b3
Linuka Arambawela <linukaarambawela@gmail.com>
2024-10-19 19:13:14 +0530

    First Commit

---
+++ src/main/java/com/linuka/OnlineTicketing/OnlineTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.linuka.OnlineTicketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineTicketingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linuka/OnlineTicketing/OnlineTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linuka.OnlineTicketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
